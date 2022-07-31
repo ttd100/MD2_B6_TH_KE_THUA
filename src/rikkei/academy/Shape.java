7b5c8a1d5641bfb3a7ec9464faa06718a2f63665
Your Name <dat054760@gmail.com>
2022-07-31 14:31:37 +0700

    up

---
+++ src/rikkei/academy/Shape.java
@@ -0,0 +1,36 @@
+package rikkei.academy;
+
+public class Shape {
+    private String color = "green";
+    private boolean filled = true ;
+    public Shape(){
+    }
+    public Shape(String color, boolean filled){
+        this.color = color;
+        this.filled =filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
